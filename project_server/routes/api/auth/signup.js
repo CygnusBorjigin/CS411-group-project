fcf54bbb274dac0153672d5b5d459094803ea345
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-11-16 20:08:28 -0500

    finished user registeration

--- project_server/routes/api/auth/signup.js
+++ project_server/routes/api/auth/signup.js
@@ -1,9 +1,28 @@
 // import frameworks
+'use strict';
 const express = require('express');
 const signup = express.Router();
+const {readFileSync, writeFileSync} = require('fs');
 
 signup.get('/', (req, res) => {
     res.send('this is for signup');
+});
+
+signup.post('/', async (req, res) => {
+    const {name, password} = req.body;
+    let userFile = JSON.parse(readFileSync('./routes/api/auth/userInfo.json', 'utf8'));
+    if (name in Object.keys(userFile)){
+        res.send("user already exist");
+        return
+    }
+    userFile[name] = {
+        password: password,
+        moviesLiked: [],
+        friends: []
+    }
+    writeFileSync('./routes/api/auth/userInfo.json', JSON.stringify(userFile, null, 2), 'utf8');
+    res.send('user registered');
+
 })
 
 module.exports = signup;
