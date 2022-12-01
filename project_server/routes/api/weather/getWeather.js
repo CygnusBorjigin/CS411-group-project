5855fe1b37f70709e0a54197f7645b40262517a9
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-12-01 16:06:58 -0500

    server
    finish weahter information api

---
+++ project_server/routes/api/weather/getWeather.js
@@ -0,0 +1,28 @@
+// import frameworks
+const axios = require("axios");
+const express = require("express");
+const weather = express.Router();
+
+
+
+const getWeather = async() => {
+    const config = {
+        method: 'get',
+        url: 'https://api.weather.gov/gridpoints/ALY/72,10/forecast',
+        headers: { }
+    };
+
+    const response = await axios(config);
+    let res = []
+    response.data.properties.periods.forEach((each, index) => {
+       if (index % 2 == 0) {
+           res.push({
+               time: each.startTime.substring(0, 10),
+               temperature: each.temperature,
+               condition: each.shortForecast
+           })
+       }
+    });
+
+    console.log(res);
+}
