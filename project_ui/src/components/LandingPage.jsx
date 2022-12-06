fed696a34d79ae970b3dc6a85e6ef26496d9a38d
Divyangna Bamalwa <75412661+divzbam@users.noreply.github.com>
2022-12-06 13:27:24 -0500

    Update LandingPage.jsx

--- project_ui/src/components/LandingPage.jsx
+++ project_ui/src/components/LandingPage.jsx
@@ -5,14 +5,14 @@ const LandingPage = () => {
             <div>
                 <h1 className={"text-center font-quicksand text-[80px]"}>CS 411 Group Project</h1>
                 <div className={"w-screen flex flex-col"}>
-                    <h1 className={"font-raleway text-center mt-6 text-[60px]"}>Group member:</h1>
+                    <h1 className={"font-raleway text-center mt-6 text-[60px]"}>Group Members:</h1>
                     <ul className={"font-raleway mx-auto mt-2 pl-10 text-[40px]"}>
                         <li>Adena Etaat</li>
                         <li>Ariel Uribe</li>
                         <li>Aleks Sekulovski</li>
                         <li>Brian Li</li>
                         <li>Cygnus Wang</li>
-                        <li>Divya Bamalwa</li>
+                        <li>Divyangna Bamalwa</li>
                     </ul>
 
                     <Link to={"/signin"} className={"mt-10 border-4 border-gray-400 w-1/6 mx-auto rounded-xl text-[20px] font-raleway hover:bg-gray-400 text-center"}>
