3d195040c5d543115934b849cc68bcae4897a2e2
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-12-06 10:16:28 -0500

    finish weather information display

--- project_ui/src/components/Dashboard.jsx
+++ project_ui/src/components/Dashboard.jsx
@@ -3918,6 +3918,7 @@ const Dashboard = () => {
         }
     ])
     const [currentDisplay, setCurrentDisplay] = useState(0);
+    const [weather, setWeather] = useState([{"time": "loading", "temperature": "loading", "condition": "loading"}]);
     const fetchMovieList = () => {
         var config = {
             method: 'get',
@@ -3934,9 +3935,27 @@ const Dashboard = () => {
             });
     };
 
+    const fetchWeather = () => {
+        var config = {
+            method: 'get',
+            url: 'http://localhost:3030/api/weather',
+            headers: { }
+        };
+
+        axios(config)
+            .then(function (response) {
+                setWeather(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+    }
+
     useEffect(() => {
         //fetchMovieList();
         setCurrentDisplay(0);
+        fetchWeather();
     }, []);
 
     const handleLike = () => {
@@ -3960,8 +3979,22 @@ const Dashboard = () => {
     }
 
     return(
-        <div className={"bg-gray-300 h-screen"}>
-            <div className={"w-screen flex justify-center pt-32"}>
+        <div className={"bg-gray-300 h-screen pt-4"}>
+            <div className="flex mx-8 border-2 border-gray-700 p-2">
+                {weather.map(each => {
+                    return(
+                        <div className={"flex border-2 border-gray-700 rounded-lg mx-2"}>
+                            <div className={"flex flex-col basis-1/3  border-r-2 border-gray-700"}>
+                                <p className={"m-2"}>{each.time.substring(5, 7)}</p>
+                                <p className={"m-2"}>{each.time.substring(8)}</p>
+                            </div>
+                            <p className={"m-2 basis-1/3"}>{each.temperature} F</p>
+                            <p className={"m-2 basis-1/3"}>{each.condition}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className={"w-screen flex justify-center mt-8"}>
                 <button className={"w-1/12 h-2/3 border-2 border-gray-700  rounded-xl my-auto mx-6 font-raleway text-[20px] font-bold hover:bg-gray-600 hover:text-white"} onClick={handleLike}>Like</button>
                 <div className={"w-1/3 h-2/3 border-2 border-gray-700 rounded-xl my-auto flex flex-col justify-center font-quicksand font-bold text-[20px]"}>
                     <h1 className={"mb-8 text-center"}>{allMovies[currentDisplay].original_title}</h1>
