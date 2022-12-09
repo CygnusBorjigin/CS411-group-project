d700e1434b5fd7bb80f88b20e2a98a9b04cfd9ef
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-12-09 08:40:37 -0500

    initialized auth server

---
+++ project_server/routes/api/auth/oauth.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const fetch = (...args) => import("node-fetch").then(({default: fetch}) => fetch(...args));
+const bodyParser = require('body-parser');
+const configFile = require("../../../../../config.json");
+const { json } = require("express");
+
+const oauth = express();
+oauth.use(cors())
+oauth.use(bodyParser.json())
+
+oauth.get('/getAccessToken', async (req, res) => {
+    console.log(req)
+    const params = `?client_id=${configFile.oauthKey.clientID}&client_secret=${configFile.oauthKey.clientSecret}&code=${req.query.code}`
+    await fetch("https://github.com/login/oauth/access_token" + params, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        res.json(data);
+    })
+})
+
+oauth.get('/getUserData', async (req, res) => {
+    req.get("Authorization");
+    await fetch("https://api.github.com/user", {
+        method: "GET",
+        headers: {
+            "Authorization": req.get("Authorization")
+        }
+    }).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        res.json(data);
+    })
+})
+
+oauth.listen(3002, function () {
+    console.log("This server is running")
+})
