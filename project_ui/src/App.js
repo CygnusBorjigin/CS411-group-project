a2864ade26e1ee9d019a6899e87498e083a69eb8
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-12-09 10:38:25 -0500

    finish github with github

--- project_ui/src/App.js
+++ project_ui/src/App.js
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 import SignIn from "./components/SignIn";
-import SignUp from "./components/SignUp";
 import Dashboard from "./components/Dashboard";
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
         <Routes>
             <Route path={"/"} element={<LandingPage />}></Route>
             <Route path={"/signin"} element={<SignIn />}></Route>
-            <Route path={"/register"} element={<SignUp />}></Route>
             <Route path={"/dashboard"} element={<Dashboard />}></Route>
         </Routes>
     )
--- project_ui/src/components/SignIn.jsx
+++ project_ui/src/components/SignIn.jsx
@@ -1,85 +1,38 @@
-import {useEffect, useState} from "react";
-import {Link, useNavigate} from "react-router-dom";
-import axios from "axios";
+import {useState, useEffect} from "react";
+const SignIn = (props) => {
+    const buttonStyle = "border-4 border-gray-500 rounded-2xl w-1/5 mx-auto hover:bg-gray-700 hover:text-black";
 
-const SignIn = () => {
-    const inputStyle = "w-2/3 h-10 border-2 rounded-md border-black mt-8 ml-auto mr-auto bg-transparent text-center text-l text-gray-600 placeholder:text-black placeholder:font-quicksand focus:outline-gray-600";
-    const buttonStyle = "text-center border-2 rounded-md border-gray-700 w-1/6 ml-auto mr-auto mt-8 text-gray-700 py-2 hover:bg-amber-50 hover:text-gray-600 font-raleway";
-    const backgroundStyle = "h-screen w-screen bg-gray-200 flex justify-center";
-    const formStyle = "h-1/2 w-1/2 border-2 border-gray-700 rounded-md mt-60 flex flex-col";
-    const titleStyle = "text-center text-black mt-8 text-2xl font-raleway";
-    const lineStyle = "mt-8 border-dotted w-2/3 ml-auto mr-auto bg-black border-1.5";
+    useEffect ( () => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString)
+        const codeParam = urlParams.get("code");
 
-    const [userName, setUserName] = useState("");
-    const [userPassword, setUserPassword] = useState("");
-    const [incorrectPassword, setIncorrectPassword] = useState(false);
+        if(codeParam && (localStorage.getItem("accessToken") === null)){
+            const getAccessToken = async () => {
+                await fetch("http://localhost:3002/getAccessToken?code=" + codeParam, {
+                    method: "GET"
+                }).then((response) => {
+                    return response.json();
+                }).then((data) => {
+                    if(data.access_token){
 
-    const navigate = useNavigate();
-
-    const handelSignIn = async () => {
-        const data = JSON.stringify({
-            "name": userName,
-            "password": userPassword
-        });
-
-        const config = {
-            method: 'post',
-            url: 'http://localhost:3030/api/auth/signin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            data : data
-        };
-
-        try {
-            const authRes = await axios(config)
-            if (authRes.data != false) {
-               localStorage.setItem("411Project", authRes.data);
-               navigate('/dashboard');
-            } else {
-                setIncorrectPassword(true);
+                        localStorage.setItem("accessToken", data.access_token);
+                    }
+                })
             }
-        } catch (error) {
-            console.log(error)
+            getAccessToken();
         }
+    }, []);
 
+    const githubLogin = () => {
+        window.location.assign("https://github.com/login/oauth/authorize?scope=user&client_id=Iv1.4378334f0e38ce91");
     }
 
-
-    const handelChange = (event) => {
-        if (event.target.id === "emailInput") setUserName(event.target.value);
-        if (event.target.id === "passwordInput") setUserPassword(event.target.value);
-    };
     return(
-        <div className={backgroundStyle}>
-            <div className={formStyle}>
-                <h1 className={titleStyle}>Sign In</h1>
-                <input
-                    id={"emailInput"}
-                    className={inputStyle}
-                    placeholder={"Please Enter your user name"}
-                    onChange={handelChange}
-                    value={userName}
-                ></input>
-                <input
-                    id={"passwordInput"}
-                    className={inputStyle}
-                    placeholder={"Please Enter your password"}
-                    onChange={handelChange}
-                    value={userPassword}
-                    type={"password"}
-                ></input>
-                <button
-                    className={buttonStyle}
-                    onClick={handelSignIn}
-                >
-                    Sign In
-                </button>
-
-                <hr className={lineStyle} />
-                <Link to={"/register"} className={buttonStyle}>Sign Up</Link>
-                {incorrectPassword ? <h1 className={"text-center font-xl text-red-700 mt-4"}>Wrong Password</h1> : null}
-            </div>
+        <div className={"w-screen h-screen flex flex-col justify-center"}>
+            <button className={buttonStyle} onClick={githubLogin}>
+                Login with GitHub
+            </button>
         </div>
     )
 }
